28298bd73678bf92451877da6bc08bcfab40685a
Rafee Ahmed <rafeeahmed@Rafees-MacBook-Pro.local>
2024-12-01 20:58:35 +0000

    updated application.yml with configuration files fo kafka and postgres and localhost

--- transaction-service/src/main/java/com/realtimefraudmonitoring/transactionservice/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/realtimefraudmonitoring/transactionservice/TransactionServiceApplication.java
@@ -2,8 +2,9 @@ package com.realtimefraudmonitoring.transactionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TransactionServiceApplication {
 
 	public static void main(String[] args) {
